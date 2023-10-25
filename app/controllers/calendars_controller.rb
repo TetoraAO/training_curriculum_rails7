cbabd36054f9fc5c1053c551e60beb2a0b091d4b
TetoraAO <kob.617.dai@gmail.com>
2023-10-25 17:44:10 +0900

    issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,15 +7,15 @@ class CalendarsController < ApplicationController
   end
 
   # 予定の保存
-  def create_week
-    Plan.create_week(plan_params)
+  def create
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit( :date, :plan)
   end
 
   def getWeek_check
